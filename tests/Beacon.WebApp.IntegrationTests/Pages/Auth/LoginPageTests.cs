b1550a96d8dffee57c9dd7499537010915d1ed95
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-05-27 11:18:32 -0400

    use mockhttp

--- tests/Beacon.WebApp.IntegrationTests/Pages/Auth/LoginPageTests.cs
+++ tests/Beacon.WebApp.IntegrationTests/Pages/Auth/LoginPageTests.cs
@@ -1,69 +1,60 @@
 using Beacon.Common.Auth;
 using Beacon.Common.Auth.Login;
-using Beacon.Common.Responses;
 using BeaconUI.Core.Auth;
 using BeaconUI.Core.Pages.Auth;
 using Bunit.TestDoubles;
 using Microsoft.Extensions.DependencyInjection;
-using Moq;
+using RichardSzalay.MockHttp;
 
 namespace Beacon.WebApp.IntegrationTests.Pages.Auth;
 
 public class LoginPageTests : TestContext
 {
     [Fact]
-    public void GivenValidCredentials_WhenLoginIsClicked_ThenRedirectToHome()
+    public async Task GivenValidCredentials_WhenLoginIsClicked_ThenRedirectToHome()
     {
         // Arrange:
-        var mockAuthClient = new Mock<BeaconAuthClient>();
-        mockAuthClient
-            .Setup(x => x.Login(It.IsAny<LoginRequest>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new UserDto
-            {
-                Id = Guid.NewGuid(),
-                EmailAddress = "test@test.com",
-                DisplayName = "test"
-            });
+        var mockHttp = Services.AddMockHttpClient();
+        mockHttp.When(HttpMethod.Post, "/api/auth/login").ThenRespondOK(new UserDto
+        {
+            Id = Guid.NewGuid(),
+            EmailAddress = "test@test.com",
+            DisplayName = "test"
+        });
 
-        Services.AddScoped(_ => mockAuthClient.Object);
-
+        Services.AddScoped<BeaconAuthClient>();
         var navManager = Services.GetRequiredService<FakeNavigationManager>();
         var cut = RenderComponent<LoginPage>();
 
         // Act:
         cut.Find("input[type=email]").Change("test@test.com");
         cut.Find("input[type=password]").Change("password123");
-        cut.Find("form").Submit();
+        await cut.Find("form").SubmitAsync();
 
         // Assert:
         navManager.History.Should().ContainSingle().Which.Uri.Should().Be("");
     }
 
     [Fact]
-    public void GivenInvalidCredentials_WhenLoginIsClicked_ThenDisplayError()
+    public async Task GivenInvalidCredentials_WhenLoginIsClicked_ThenDisplayError()
     {
         // Arrange:
-        var mockAuthClient = new Mock<BeaconAuthClient>();
-        mockAuthClient
-            .Setup(x => x.Login(It.IsAny<LoginRequest>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new ValidationProblemResponse
-            {
-                Errors = new Dictionary<string, string[]>
-                {
-                    { nameof(LoginRequest.EmailAddress), new[] { "Some error message" } }
-                }
-            });
+        var mockHttp = Services.AddMockHttpClient();
+        mockHttp.When(HttpMethod.Post, "/api/auth/login").ThenRespondValidationProblem(new()
+        {
+            { nameof(LoginRequest.EmailAddress), new[] { "Some error message" } }
+        });
 
-        Services.AddScoped(_ => mockAuthClient.Object);
-
+        Services.AddScoped<BeaconAuthClient>();
         var navManager = Services.GetRequiredService<FakeNavigationManager>();
         var cut = RenderComponent<LoginPage>();
 
         // Act:
         cut.Find("input[type=email]").Change("test@test.com");
         cut.Find("input[type=password]").Change("password123");
-        cut.Find("form").Submit();
+        await cut.Find("form").SubmitAsync();
 
+        // Assert:
         navManager.History.Should().BeEmpty();
     }
 }
