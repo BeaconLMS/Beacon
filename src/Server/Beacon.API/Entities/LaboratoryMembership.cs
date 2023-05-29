00736f0f54c104ad4ab27c6c25b43a633be4ee2b
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-05-29 16:06:20 -0400

    add migration

---
+++ src/Server/Beacon.API/Entities/LaboratoryMembership.cs
@@ -0,0 +1,10 @@
+﻿namespace Beacon.API.Entities;
+
+public class LaboratoryMembership
+{
+    public required Guid Id { get; init; }
+    public required LaboratoryMembershipType MembershipType { get; init; }
+
+    public required Laboratory Laboratory { get; set; }
+    public required User Member { get; set; }
+}
--- src/Server/Beacon.API/Persistence/BeaconDbContext.cs
+++ src/Server/Beacon.API/Persistence/BeaconDbContext.cs
@@ -5,6 +5,8 @@ namespace Beacon.API.Persistence;
 
 public class BeaconDbContext : DbContext
 {
+    public DbSet<Laboratory> Laboratories => Set<Laboratory>();
+    public DbSet<LaboratoryMembership> LaboratoryMemberships => Set<LaboratoryMembership>();
     public DbSet<User> Users => Set<User>();
 
     public BeaconDbContext(DbContextOptions<BeaconDbContext> options) : base(options)
@@ -13,10 +15,25 @@ public class BeaconDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Laboratory>(builder =>
+        {
+            builder.Property(x => x.Name).HasMaxLength(50);
+            builder.Property(x => x.Slug).HasMaxLength(20);
+            builder.HasIndex(x => x.Slug).IsUnique();
+        });
+
+        modelBuilder.Entity<LaboratoryMembership>(builder =>
+        {
+            builder.Property(x => x.MembershipType).HasConversion<string>().HasMaxLength(20);
+            builder.HasOne(x => x.Laboratory).WithMany(x => x.Memberships);
+            builder.HasOne(x => x.Member).WithMany(x => x.Memberships);
+            builder.HasIndex("LaboratoryId", "MemberId").IsUnique();
+        });
+
         modelBuilder.Entity<User>(builder =>
         {
-            builder.HasIndex(x => x.EmailAddress).IsUnique();
             builder.Property(x => x.EmailAddress).HasMaxLength(255);
+            builder.HasIndex(x => x.EmailAddress).IsUnique();
         });
     }
 }
