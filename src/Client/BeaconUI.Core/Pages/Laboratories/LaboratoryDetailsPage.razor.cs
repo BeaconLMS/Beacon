48803b30dc697f5e5a6aec24f1e381a6b907eb9f
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-06-06 19:17:28 -0400

    more formatting

--- src/Client/BeaconUI.Core/Pages/Laboratories/LaboratoryDetailsPage.razor.cs
+++ src/Client/BeaconUI.Core/Pages/Laboratories/LaboratoryDetailsPage.razor.cs
@@ -1,7 +1,4 @@
 using Beacon.Common.Laboratories;
-using BeaconUI.Core.Shared.Laboratories;
-using Blazored.Modal;
-using Blazored.Modal.Services;
 using Microsoft.AspNetCore.Components;
 
 namespace BeaconUI.Core.Pages.Laboratories;
@@ -10,15 +7,5 @@ public partial class LaboratoryDetailsPage
 {
     [CascadingParameter] public required LaboratoryDetailDto Details { get; set; }
 
-    [CascadingParameter] private IModalService ModalService { get; set; } = null!;
-
     [Parameter] public required Guid Id { get; set; }
-
-    private async Task ShowInviteMemberModal()
-    {
-        var modalParameters = new ModalParameters()
-            .Add(nameof(InviteMemberForm.LaboratoryId), Id);
-
-        await ModalService.Show<InviteMemberForm>("Invite Lab Member", modalParameters).Result;
-    }
 }
