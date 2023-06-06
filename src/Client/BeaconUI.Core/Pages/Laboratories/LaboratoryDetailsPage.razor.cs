dff52a7321cf304aa44f90e02919589a250168df
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-06-06 17:49:38 -0400

    cascade down current lab details from layout

--- src/Client/BeaconUI.Core/Pages/Laboratories/LaboratoryDetailsPage.razor.cs
+++ src/Client/BeaconUI.Core/Pages/Laboratories/LaboratoryDetailsPage.razor.cs
@@ -1,5 +1,4 @@
 using Beacon.Common.Laboratories;
-using BeaconUI.Core.Clients;
 using BeaconUI.Core.Shared.Laboratories;
 using Blazored.Modal;
 using Blazored.Modal.Services;
@@ -9,25 +8,11 @@ namespace BeaconUI.Core.Pages.Laboratories;
 
 public partial class LaboratoryDetailsPage
 {
-    [Inject] private LabClient LabClient { get; set; } = null!;
+    [CascadingParameter] public required LaboratoryDetailDto Details { get; set; }
 
     [CascadingParameter] private IModalService ModalService { get; set; } = null!;
 
     [Parameter] public required Guid Id { get; set; }
-
-    private LaboratoryDetailDto? Details { get; set; }
-
-    protected override async Task OnParametersSetAsync()
-    {
-        if (Details?.Id != Id)
-            await LoadLabDetails();
-    }
-
-    private async Task LoadLabDetails()
-    {
-        var result = await LabClient.GetLaboratoryDetailsAsync(Id);
-        Details = result.IsError ? null : result.Value;        
-    }
 
     private async Task ShowInviteMemberModal()
     {
