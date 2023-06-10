dea70e52f912cbce02f83eb3b0f83a139fa7d71c
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-06-10 10:04:15 -0400

    wip

--- src/Shared/Beacon.Common/Laboratories/LaboratoryMemberDto.cs
+++ src/Shared/Beacon.Common/Laboratories/LaboratoryMemberDto.cs
@@ -8,4 +8,11 @@ public record LaboratoryMemberDto
     public required string DisplayName { get; init; }
     public required string EmailAddress { get; init; }
     public required LaboratoryMembershipType MembershipType { get; init; }
+
+    public UserDto ToUserDto() => new()
+    {
+        Id = Id,
+        DisplayName = DisplayName,
+        EmailAddress = EmailAddress
+    };
 }
