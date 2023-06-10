49bc751e753a9a7db82820985ddb3f02820ec4e8
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-06-10 12:12:02 -0400

    maaaybe?

--- tests/Beacon.WebApp.IntegrationTests/BeaconTestContext.cs
+++ tests/Beacon.WebApp.IntegrationTests/BeaconTestContext.cs
@@ -8,5 +8,6 @@ public abstract class BeaconTestContext : TestContext
     {
         Services.AddBeaconUI();
         JSInterop.SetupModule("./_content/Blazored.Modal/BlazoredModal.razor.js");
+        JSInterop.SetupModule("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js");
     }
 }
